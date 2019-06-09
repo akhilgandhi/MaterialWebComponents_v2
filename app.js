3781e83637e57c280deeb5f1ca0bb747e2b78e46
akhilgandhi <akhilgandhi@live.in>
2019-06-09 20:01:38 +0530

    Added responsive behavior to Drawer and Top App Bar

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { MDCDrawer } from "@material/drawer";
 import { MDCTopAppBar } from '@material/top-app-bar';
+import {MDCRipple} from '@material/ripple';
 
 // Instantiation
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
@@ -7,4 +8,8 @@ const topAppBar = new MDCTopAppBar(topAppBarElement);
 const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
 topAppBar.listen('MDCTopAppBar:nav', () => {
     drawer.open = !drawer.open;
+});
+const selector = '.mdc-button, .mdc-list-item';
+const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
+  return new MDCRipple(el);
 });
